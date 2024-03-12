54be626d438fbdffb44ea600433434be7cd25d07
bodhis96 <35863988+bodhis96@users.noreply.github.com>
2024-03-12 17:29:06 +0530

    Update ap_invoice_creation_summary_source_vs_period.sql

--- pivot_vs_no_pivot/ap_invoice_creation_summary_source_vs_period.sql
+++ pivot_vs_no_pivot/ap_invoice_creation_summary_source_vs_period.sql
@@ -1,23 +1,5 @@
 -- Count of AP Invoices created from all sources (along rows) and for specifeid periods (along columns)
--- Done without using PIVOT keyword
-SELECT
-    source,
-    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2023-12',1,0))) AS creation_period_2023_12,
-    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2024-01',1,0))) AS creation_period_2024_01,
-    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2024-02',1,0))) AS creation_period_2024_02,
-    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2025-03',1,0))) AS creation_period_2024_03,
-    count(invoice_id) AS count_invoices
-FROM
-    ap_invoices_all
-GROUP BY
-    source
-;
-
-
-
-
--- Count of AP Invoices created from all sources (along rows) and for specifeid periods (along columns)
--- Done by using PIVOT keyword
+-- PIVOT
 SELECT
     source,
     creation_period_2023_12,
@@ -41,3 +23,20 @@ FROM
             '2024-03' AS creation_period_2024_03,
         )
     )
+;
+
+
+-- Count of AP Invoices created from all sources (along rows) and for specifeid periods (along columns)
+-- NO PIVOT
+SELECT
+    source,
+    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2023-12',1,0))) AS creation_period_2023_12,
+    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2024-01',1,0))) AS creation_period_2024_01,
+    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2024-02',1,0))) AS creation_period_2024_02,
+    sum(DECODE(TO_CHAR(creation_date, 'YYYY-MM','2025-03',1,0))) AS creation_period_2024_03,
+    count(invoice_id) AS count_invoices
+FROM
+    ap_invoices_all
+GROUP BY
+    source
+;
